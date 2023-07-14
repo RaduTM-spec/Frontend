5bb121dae99f151d9cce0aa8a6c4a492cb9a2656
Andrei-David Nan <nandavid190@gmail.com>
2023-07-14 20:22:36 +0300

    v0.1.2 Configured app and dashboard component structure;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { UserBoxComponent } from './user-box/user-box.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     UserBoxComponent,
+    DashboardComponent,
 
   ],
   imports: [
