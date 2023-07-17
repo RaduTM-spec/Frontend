35145f44349476ada149a684bfb3d710577f98dc
Andrei-David Nan <nandavid190@gmail.com>
2023-07-17 19:04:32 +0300

    v0.2 commit with the changes to side panel and cards;

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -32,48 +32,48 @@ export class DashboardComponent {
   }
 
 
-
-  showJoinActivityModal() {
-    const joinActivityModal = this.joinActivityModalRef.nativeElement.querySelector('#joinActivityModal');
-    $(joinActivityModal).modal('show');
-
-    // Close modal when clicking outside or on close button (x)
-    this.renderer.listen(joinActivityModal, 'click', (event) => {
-      if (
-        event.target === joinActivityModal ||
-        event.target.classList.contains('close') ||
-        event.target.classList.contains('btn') &&
-        event.target.classList.contains('btn-secondary')
-      ) {
-        $(joinActivityModal).modal('hide');
-      }
-    });
-  }
-
-  showCreateActivityModal() {
-    const createActivityModal = this.createActivityModalRef.nativeElement.querySelector('#createActivityModal');
-    $(createActivityModal).modal('show');
-
-    // Close modal when clicking outside or on close button (x)
-    this.renderer.listen(createActivityModal, 'click', (event) => {
-      if (
-        event.target === createActivityModal ||
-        event.target.classList.contains('close') ||
-        event.target.classList.contains('btn') &&
-        event.target.classList.contains('btn-secondary')
-      ) {
-        $(createActivityModal).modal('hide');
-      }
-    });
-  }
-
-
-  closeModalOnOutsideClick() {
-    const modalBackdrop = document.getElementsByClassName('modal-backdrop');
-    if (modalBackdrop && modalBackdrop.length > 0) {
-      this.renderer.removeChild(document.body, modalBackdrop[0]);
-    }
-  }
+  //
+  // showJoinActivityModal() {
+  //   const joinActivityModal = this.joinActivityModalRef.nativeElement.querySelector('#joinActivityModal');
+  //   $(joinActivityModal).modal('show');
+  //
+  //   // Close modal when clicking outside or on close button (x)
+  //   this.renderer.listen(joinActivityModal, 'click', (event) => {
+  //     if (
+  //       event.target === joinActivityModal ||
+  //       event.target.classList.contains('close') ||
+  //       event.target.classList.contains('btn') &&
+  //       event.target.classList.contains('btn-secondary')
+  //     ) {
+  //       $(joinActivityModal).modal('hide');
+  //     }
+  //   });
+  // }
+  //
+  // showCreateActivityModal() {
+  //   const createActivityModal = this.createActivityModalRef.nativeElement.querySelector('#createActivityModal');
+  //   $(createActivityModal).modal('show');
+  //
+  //   // Close modal when clicking outside or on close button (x)
+  //   this.renderer.listen(createActivityModal, 'click', (event) => {
+  //     if (
+  //       event.target === createActivityModal ||
+  //       event.target.classList.contains('close') ||
+  //       event.target.classList.contains('btn') &&
+  //       event.target.classList.contains('btn-secondary')
+  //     ) {
+  //       $(createActivityModal).modal('hide');
+  //     }
+  //   });
+  // }
+  //
+  //
+  // closeModalOnOutsideClick() {
+  //   const modalBackdrop = document.getElementsByClassName('modal-backdrop');
+  //   if (modalBackdrop && modalBackdrop.length > 0) {
+  //     this.renderer.removeChild(document.body, modalBackdrop[0]);
+  //   }
+  // }
 
 
   exportSituation() {
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Renderer2 } from '@angular/core';
+import { ElementRef } from '@angular/core';
+
+declare var $: any; // Declared the $ symbol from jQuery
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +10,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+
+  joinActivityModalRef: ElementRef;
+  createActivityModalRef: ElementRef;
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {
+    this.joinActivityModalRef = this.elementRef;
+    this.createActivityModalRef = this.elementRef;
+  }
+
+
+
+  showJoinActivityModal() {
+    const joinActivityModal = this.joinActivityModalRef.nativeElement.querySelector('#joinActivityModal');
+    $(joinActivityModal).modal('show');
+
+    // Close modal when clicking outside or on close button (x)
+    this.renderer.listen(joinActivityModal, 'click', (event) => {
+      if (
+        event.target === joinActivityModal ||
+        event.target.classList.contains('close') ||
+        event.target.classList.contains('btn') &&
+        event.target.classList.contains('btn-secondary')
+      ) {
+        $(joinActivityModal).modal('hide');
+      }
+    });
+  }
+
+  showCreateActivityModal() {
+    const createActivityModal = this.createActivityModalRef.nativeElement.querySelector('#createActivityModal');
+    $(createActivityModal).modal('show');
+
+    // Close modal when clicking outside or on close button (x)
+    this.renderer.listen(createActivityModal, 'click', (event) => {
+      if (
+        event.target === createActivityModal ||
+        event.target.classList.contains('close') ||
+        event.target.classList.contains('btn') &&
+        event.target.classList.contains('btn-secondary')
+      ) {
+        $(createActivityModal).modal('hide');
+      }
+    });
+  }
+
+
+  closeModalOnOutsideClick() {
+    const modalBackdrop = document.getElementsByClassName('modal-backdrop');
+    if (modalBackdrop && modalBackdrop.length > 0) {
+      this.renderer.removeChild(document.body, modalBackdrop[0]);
+    }
+  }
 
 }
