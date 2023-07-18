26ea3539995df0216e00c76c6d981f5fc534ea8e
Andrei-David Nan <nandavid190@gmail.com>
2023-07-18 21:20:02 +0300

    v0.2.5 made mock-members in user dashboard, added buttons and badges in dashboard;

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -23,7 +23,54 @@ export class DashboardComponent {
   isMember: boolean = false;
   isLead: boolean = false;
   teams: any[] = [];
-  members: User[] = [];
+  members: User[] = [{
+    id: 1,
+    username: "john_doe",
+    role: "TEAM LEAD",
+    attendances: 20,
+    grade: 9.5,
+    imageUrl: "https://robohash.org/john_doe?bgset=bg1",
+  },
+    {
+      id: 2,
+      username: "jane_smith",
+      role: "MEMBER",
+      attendances: 15,
+      grade: 8.5,
+      imageUrl: "https://robohash.org/bruhdh?bgset=bg1",
+    },
+    {
+      id: 3,
+      username: "mike_johnson",
+      role: "MEMBER",
+      attendances: 10,
+      grade: 7.5,
+      imageUrl: "https://robohash.org/asdasdaaaa?bgset=bg1",
+    },
+    {
+      id: 4,
+      username: "bob_williams",
+      role: "MEMBER",
+      attendances: 5,
+      grade: 4.5,
+      imageUrl: "https://robohash.org/asdasdasd?bgset=bg1",
+    },
+    {
+      id: 5,
+      username: "david_brown",
+      role: "MEMBER",
+      attendances: 12,
+      grade: 9.5,
+      imageUrl: "https://robohash.org/hehehe?bgset=bg1",
+    },
+    {
+      id: 6,
+      username: "emily_clark",
+      role: "MEMBER",
+      attendances: 8,
+      grade: 6.5,
+      imageUrl: "https://robohash.org/idk-bruh-idk?bgset=bg1",
+    },];
   activityName: String = "";
 
   joinActivityModalRef: ElementRef;
